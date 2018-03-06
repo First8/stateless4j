b08b78796bb1dc7c6205e72b49856ff55d214b7c
ekaekebeke <e.kaekebeke@first8.nl>
2018-03-06 10:56:50 +0100

    Added test for onExit property change

--- src/test/java/com/github/oxo42/stateless4j/ContextedStateMachineTests.java
+++ src/test/java/com/github/oxo42/stateless4j/ContextedStateMachineTests.java
@@ -41,4 +41,24 @@ public class ContextedStateMachineTests {
 
         Assert.assertEquals(null, contextedStateMachine.getStateMachineContext().getAttribute("test-key"));
     }
+
+    @Test
+    public void OnExitSuccessfullyUpdatesContext() {
+        StateMachineConfig<State, Trigger> config = new StateMachineConfig<>();
+
+        config.configure(State.A)
+                .permit(Trigger.X, State.B);
+
+        config.configure(State.A)
+                .onExit(((arg1, arg2) -> arg1.setAttribute("test-key", "123")), DefaultStateMachineContext.class);
+
+        ContextedStateMachine<State, Trigger> contextedStateMachine = new ContextedStateMachine<>(State.A, config);
+
+        Assert.assertEquals(null, contextedStateMachine.getStateMachineContext().getAttribute("test-key"));
+
+        contextedStateMachine.fire(Trigger.X, contextedStateMachine.getStateMachineContext());
+
+        Assert.assertEquals(1, contextedStateMachine.getStateMachineContext().attributes().count());
+        Assert.assertEquals("123", contextedStateMachine.getStateMachineContext().getAttribute("test-key"));
+    }
 }
