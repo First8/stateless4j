0d87fdaff1ca474e20eb5a842f5438c6a78012ff
basp <b.passon@first8.nl>
2018-03-20 09:20:51 +0100

    added context as default param to action

--- src/main/java/com/github/oxo42/stateless4j/delegates/Action.java
+++ src/main/java/com/github/oxo42/stateless4j/delegates/Action.java
@@ -1,12 +1,14 @@
 package com.github.oxo42.stateless4j.delegates;
 
+import com.github.oxo42.stateless4j.StateMachineContext;
+
 /**
  * Represents an operation that accepts no input arguments and returns no result.
  */
-public interface Action {
+public interface Action<S,T> {
 
     /**
      * Performs this operation
      */
-    void doIt();
+    void doIt( StateMachineContext<S,T> context );
 }
